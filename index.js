75de8a11aa761ac3ca54740e2c3fd7ec4f0f64ee
Justin <justin.i7394@gmail.com>
2022-03-21 15:41:40 -0400

    commit tree 95f860c83e8094e4175e84d5335708f184d07caa

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+// Code your solutions in this file 
+
+function writeCards(cards, event) {
+    const messages = []; 
+    for (let j = 0; j < cards.length; j++) {
+       messages.push(`Thank you, ${cards[j]}, for the wonderful ${event} gift!`) 
+    } 
+
+    return messages;
+} 
+
+
+// create a new array, add a message for each name into that array, 
+//after the loop is done return all array with messages 
+
+ 
+function countDown(){ 
+    let count = 11 
+
+    while (count > 0) {
+        count--; 
+        console.log(count); 
+    }  
+}
+
+console.log(countDown(10)); 
